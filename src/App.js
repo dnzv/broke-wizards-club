2a91ec0d8909c5d67065cb093ba4516f79d43223
RS38XU <deniz.vahaboglu@ing.com>
2019-10-28 14:18:45 +0100

    add spell info card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Spell from "./components/Spell";
 
 function App() {
-  return (
-    <div className="text-center">
-      <header className="m-6 p-6 rounded shadow-xl">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-base">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const name = 'Absorb Elements';
+    const description = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci dolores, ex hic laboriosam modi nostrum
+            officia provident quis rem similique. Autem dicta ea enim facilis ipsam quasi sapiente tempore ullam. Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aspernatur enim et incidunt itaque labore
+            magnam nulla! Aliquid distinctio dolores eaque eveniet fugit molestiae nesciunt, numquam officiis provident
+            rerum sed.`;
+    const footnote = '* - which you take when you take acid, cold, fire, lightning, or thunder damage';
+    const statBlock = {
+        level: 1,
+        castingTime: '1 Reaction *',
+        range: 'Self',
+        area: 'Self',
+        verbal: false,
+        somatic: true,
+        material: false,
+        duration: '1 Round',
+        school: 'Abjuration',
+        attack: null,
+        save: null,
+        damage: null,
+        damageType: [],
+        effect: null
+    };
+    const stats = [
+        { label: 'level', value: '1st' },
+        { label: 'casting time', value: '1 reaction *' },
+        { label: 'range/area', value: 'self' },
+        { label: 'components', value: 's' },
+        { label: 'duration', value: '1 round' },
+        { label: 'school', value: 'abjuration' },
+        { label: 'attack/save', value: 'none' },
+        { label: 'damage/effect', value: 'acid' },
+    ];
+    const classes = [ 'Druid', 'Ranger', 'Sorcerer', 'Wizard' ];
+    const tags = [ 'Damage', 'Warding' ];
+    return (
+        <div>
+            <Spell name={ name } description={ description } footnote={ footnote }
+                   stats={ stats } classes={ classes } tags={ tags }/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Spell.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Spell = props => (
+    <div className="m-6 px-6 pt-4 pb-6 shadow border-solid border-t-4 border-purple-600 text-gray-900">
+        <p className="text-2xl font-bold mb-6 capitalize">{ props.name }</p>
+        <div className="flex flex-wrap border-solid border-b-2 border-purple-600 mb-4">
+            { props.stats.map(stat =>
+                <div className="w-1/2 md:w-1/4 mb-4">
+                    <p className="text-sm font-bold uppercase">{ stat.label }</p><p
+                    className="text-sm capitalize">{ stat.value }</p>
+                </div>
+            ) }
+        </div>
+        <p className="text-sm text-justify mb-4">{ props.description }</p>
+        <p className="text-xs italic mb-6">{ props.footnote }</p>
+        <div className="flex flex-wrap mb-4">
+            <p className="text-sm">Tags:</p>
+            { props.tags.map(tag =>
+                <div
+                    style={ { fontSize: '0.58rem', borderColor: '#cbc6c3' } }
+                    className="font-bold uppercase mx-1 px-2 py-1
+                    border-solid border shadow-inner">{ tag }</div>
+            ) }
+        </div>
+        <div className="flex flex-wrap">
+            <p className="text-sm">Available For:</p>
+            { props.classes.map(dndClass =>
+                <div
+                    style={ { fontSize: '0.58rem', borderColor: '#cbc6c3' } }
+                    className="font-bold uppercase mx-1 px-2 py-1
+                    border-solid border shadow-inner">{ dndClass }</div>
+            ) }
+        </div>
+    </div>
+);
+
+export default Spell
